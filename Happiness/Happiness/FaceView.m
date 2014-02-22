87b8a1a9806b3b89638536b0147975016ef7a52e
Weiwei Shi <weiwei@yahoo-inc.com>
2014-02-22 22:15:01 +0800

    pan gesture

--- Happiness/Happiness/FaceView.m
+++ Happiness/Happiness/FaceView.m
@@ -20,6 +20,8 @@
 
 @implementation FaceView
 
+@synthesize dataSource = _dataSource;
+
 @synthesize scale = _scale;
 
 -(CGFloat)scale
@@ -122,7 +124,9 @@
     CGPoint mouthCP2 = mouthEnd;
     mouthCP2.x -= MOUSE_H * size * 2 / 3;
     
-    float smile = 1.0;
+    float smile = [self.dataSource smileForFaceView:self];
+    if (smile < -1) smile = -1;
+    if (smile > 1) smile = 1;
     
     CGFloat smileOffset = MOUSE_SMILE * size * smile;
     mouthCP1.y += smileOffset;
@@ -132,9 +136,6 @@
     CGContextMoveToPoint(context, mouthStart.x, mouthStart.y);
     CGContextAddCurveToPoint(context, mouthCP1.x, mouthCP1.y, mouthCP2.x, mouthCP2.y, mouthEnd.x, mouthEnd.y);
     CGContextStrokePath(context);
-    
-    
-    
 }
 
 @end
--- Happiness/Happiness/HappinessViewController.m
+++ Happiness/Happiness/HappinessViewController.m
@@ -9,7 +9,7 @@
 #import "HappinessViewController.h"
 #import "FaceView.h"
 
-@interface HappinessViewController ()
+@interface HappinessViewController () <FaceViewDataSource>
 
 @property (nonatomic, weak) IBOutlet FaceView *faceView;
 
@@ -31,11 +31,28 @@
 {
     _faceView = faceView;
     [self.faceView addGestureRecognizer:[[UIPinchGestureRecognizer alloc] initWithTarget:self.faceView action:@selector(pinch:)]];
+    [self.faceView addGestureRecognizer:[[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handleHappinessGesture:)]];
+    self.faceView.dataSource = self;
+}
+
+- (void)handleHappinessGesture:(UIPanGestureRecognizer *)gesture
+{
+    if ((gesture.state == UIGestureRecognizerStateChanged) ||
+        (gesture.state == UIGestureRecognizerStateEnded)) {
+        CGPoint translation = [gesture translationInView:self.faceView];
+        self.happiness -= translation.y / 2;
+        [gesture setTranslation:CGPointZero inView:self.faceView];
+    }
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation
 {
     return YES;
+}
+
+- (float)smileForFaceView:(FaceView *)sender
+{
+    return (self.happiness - 50) / 50.0;
 }
 
 @end
